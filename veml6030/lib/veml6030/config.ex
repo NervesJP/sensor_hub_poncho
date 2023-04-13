4393bc75d009c5da6896f35c600cd142f2ded2d4
Ryota Kinukawa <pojiro.jp@gmail.com>
2023-04-13 19:12:15 +0900

    veml6030 project を追加

---
+++ veml6030/lib/veml6030/config.ex
@@ -0,0 +1,56 @@
+defmodule VEML6030.Config do
+  @to_lumens_factor %{
+    {:it_800_ms, :gain_2x} => 0.0036,
+    {:it_800_ms, :gain_1x} => 0.0072,
+    {:it_800_ms, :gain_1_4th} => 0.0288,
+    {:it_800_ms, :gain_1_8th} => 0.0576
+  }
+
+  defstruct gain: :gain_1_4th,
+            int_time: :it_100_ms,
+            shutdown: false,
+            interrupt: false
+
+  def new, do: struct(__MODULE__)
+  def new(opts), do: struct(__MODULE__, opts)
+
+  def to_integer(config) do
+    reserved = 0
+    persistence_protect = 0
+
+    <<integer::16>> = <<
+      reserved::3,
+      gain(config.gain)::2,
+      reserved::1,
+      int_time(config.int_time)::4,
+      persistence_protect::2,
+      reserved::2,
+      interrupt(config.interrupt)::1,
+      shutdown(config.shutdown)::1
+    >>
+
+    integer
+  end
+
+  defp gain(:gain_1x), do: 0b0
+  defp gain(:gain_2x), do: 0b01
+  defp gain(:gain_1_8th), do: 0b10
+  defp gain(:gain_1_4th), do: 0b11
+  defp gain(:gain_default), do: 0b11
+
+  defp int_time(:it_25_ms), do: 0b1100
+  defp int_time(:it_50_ms), do: 0b1000
+  defp int_time(:it_100_ms), do: 0b0000
+  defp int_time(:it_200_ms), do: 0b0001
+  defp int_time(:it_400_ms), do: 0b0010
+  defp int_time(:it_800_ms), do: 0b0011
+  defp int_time(:it_default), do: 0b0000
+  defp shutdown(true), do: 1
+  defp shutdown(_), do: 0
+  defp interrupt(true), do: 1
+  defp interrupt(_), do: 0
+
+  def to_lumens(config, measurement) do
+    @to_lumens_factor[{config.int_time, config.gain}] * measurement
+  end
+end
